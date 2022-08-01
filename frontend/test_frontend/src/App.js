87f29128f376f76c3d91100513622645cad61ba2
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-01 19:03:38 +0800

    update,

--- frontend/test_frontend/src/App.js
+++ frontend/test_frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
 function App() {
+  const apiOnClick = () => {
+    console.log('helloworld');
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <div>helloworld test_frontend</div>
+        <div>helloworld test_frontend ccccc</div>
+        <button onClick={(e) => apiOnClick(e)}>/api</button>
       </header>
     </div>
   );
