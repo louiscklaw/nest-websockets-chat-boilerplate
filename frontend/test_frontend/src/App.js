888e0a489dd8660bcd72512661e4803a69072f32
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-01 19:06:49 +0800

    update working,

--- frontend/test_frontend/src/App.js
+++ frontend/test_frontend/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 
 function App() {
   const apiOnClick = () => {
-    console.log('helloworld');
+    fetch('/api')
+      .then((res) => res.json())
+      .then((res_json) => console.log({ res_json }));
   };
+  const apiHealthOnClick = () => {
+    fetch('/api/health')
+      .then((res) => res.json())
+      .then((res_json) => console.log({ res_json }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <div>helloworld test_frontend ccccc</div>
         <button onClick={(e) => apiOnClick(e)}>/api</button>
+        <button onClick={(e) => apiHealthOnClick(e)}>/api/health</button>
       </header>
     </div>
   );
